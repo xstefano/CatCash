57eec296d8a6b3642a40d21b35e45e4aa3d0ea99
Andres Stefano Alegre Llantoy <xcloudsgls@gmail.com>
2023-09-14 18:21:25 -0500

    Coleccion y contador de item

---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int fishes = 0;
+
+    [SerializeField] private TextMeshProUGUI fishesText;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Fish"))
+        {
+            Destroy(collision.gameObject);
+            fishes++;
+            fishesText.text = $"Pescados: {fishes}";
+        }
+    }
+}
