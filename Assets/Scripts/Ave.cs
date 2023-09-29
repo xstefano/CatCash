4a734a923cbeff3b74b1c5e9b2c524c1fd813583
RayXax <jean62471501@hotmail.com>
2023-09-29 01:59:15 -0500

    MOVIEMTOS

---
+++ Assets/Scripts/Ave.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ave : MonoBehaviour
+{
+    private float velocidad;
+    private float limiteX1;
+    private float limiteX2;
+    private Vector2 direccion;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    public void crear(float x, float y, float vel, bool der, float limx1, float limx2)
+    {
+        int dir;
+        transform.position = new Vector2(x, y);
+        velocidad = vel;
+        dir = (der == true ? 1 : -1);
+        direccion = new Vector2(dir, 0);
+        limiteX1 = limx1;
+        limiteX2 = limx2;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(direccion * velocidad * Time.deltaTime);
+
+        if (transform.position.x > limiteX2 && direccion.x > 0)
+        {
+
+            direccion.x = -direccion.x;
+
+        }
+        if (transform.position.x < limiteX1 && direccion.x < 0)
+        {
+
+            direccion.x = -direccion.x;
+
+        }
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public Ave ave;
+    public Dog dog;
+   
     // Start is called before the first frame update
+
     void Start()
     {
-        
+        Ave a1 = Instantiate(ave) as Ave;
+        Ave a2 = Instantiate(ave) as Ave;
+        Ave a3 = Instantiate(ave) as Ave;
+        Ave a4 = Instantiate(ave) as Ave;
+        Ave a5 = Instantiate(ave) as Ave;
+
+        a1.crear(8f, 3f, 4, true, 11f, 16f);//ave1
+        a2.crear(8f, 5f, 4, true, 7f, 16f); //ave2
+        a3.crear(8f, 8f, 4, true, 8f, 11f); //ave3
+        a4.crear(10.2f, 6.9f, 4, true, 8f, 11f); //ave3
+        a5.crear(12.7f, 5.3f, 4, true, 8f, 11f); //ave3
+
+        Dog b1 = Instantiate(dog) as Dog;
+        b1.crear(8f, 2.21f, 1, true, 11f, 15f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
